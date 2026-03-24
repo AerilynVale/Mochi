0c60f6c5259cbd8548823cd1e82dfe756db8833d
Aerilyn Vale <valeaerilyn@gmail.com>
2026-03-23 19:26:13 -0600

    Split input into command and arguments.

--- Mochi/Core/MochiCommands.cs
+++ Mochi/Core/MochiCommands.cs
@@ -6,7 +6,27 @@ class MochiCommands
 {
     internal static int ExecuteCommand(string input)
     {
-        Console.WriteLine($"Running command for {input}.");
+        var (command, args) =  ParseCommand(input);
+        Console.WriteLine($"{command} -- args:");
+
+        foreach (var arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+
         return 0;
+    }
+
+    private static (string, List<string>) ParseCommand(string input)
+    {
+        string[] parts = input.Split(' ', 2);
+
+        string command = parts[0];
+
+        List<string> args = parts.Length > 1
+            ? parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList()
+            : new List<string>();
+
+        return (command, args);
     }
 }
